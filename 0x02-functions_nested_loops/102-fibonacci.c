505adc40d6cb160fc0c1023cf0e6381beab92138
Emmanuella Ogun <ogunemmanuella2@gmail.com>
2022-09-13 15:53:27 -0700

    prints first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int first = 1, second = 2, next c;
+
+	for (c = 1; c < 50; c++)
+	{
+		if (c <= 1)
+		{
+			next = c;
+		}
+		else
+		{
+			next = first + second;
+			first = second;
+			second = next;
+		}
+		printf("%d, ", next);
+	}
+	return (0);
+}
