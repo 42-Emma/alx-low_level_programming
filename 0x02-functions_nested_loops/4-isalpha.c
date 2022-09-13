137a1a11a96585b9cad8063c1ecbbdddad9307de
Emmanuella Ogun <ogunemmanuella2@gmail.com>
2022-09-13 07:14:21 -0700

    checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * isalpha - determines if character is an alphabet
  * @c: letter being tested
  *
- * Description: 'accepts an input and checks if it is an alphabet'
+ * Description: Accepts an input and checks if it is an alphabet
  * Return: 1 for success, 0 for failure
  */
 int _isalpha(int c)
