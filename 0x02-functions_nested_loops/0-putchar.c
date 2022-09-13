007c943f94521c52f1596833b918447b33ae8f30
Emmanuella Ogun <ogunemmanuella2@gmail.com>
2022-09-13 05:56:25 -0700

    prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * Return: Always 0 for success
  */
 int main(void)
-{	
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,7 +15,4 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-
-	return (0);
 }
