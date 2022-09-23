d408b69cd8dba361f3f1f578ce5c6d55717d7d78
Emmanuella Ogun <ogunemmanuella2@gmail.com>
2022-09-23 15:22:00 -0700

    function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,24 +5,25 @@
  * @c: string to be encoded
  * Return: encoded string
  */
-char *leet(char *c)
+char *leet(char *s)
 {
 	int a = 0, b = 0, l = 5;
 	char r[5] = {'A', 'E', 'O', 'T', 'L'};
 	char n[5] = {'4', '3', '0', '7', '1'};
 
-	while (c[a])
+	while (s[a])
 	{
 		b = 0;
-		while (b < 1)
+		while (b < l)
 		{
-			if (c[a] == r[b] || c[a] - 32 == r[b])
+			if (s[a] == r[b] || s[a] - 32 == r[b])
 			{
-				c[a] = n[b]
+				s[a] = n[b];
 			}
 			b++;
 		}
-		a++
+		a++;
 	}
-	return (c);
+	return (s);
+	
 }
