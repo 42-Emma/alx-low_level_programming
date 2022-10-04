9e40c3d80affc8224a7a302926145d770a974b9a
Emmanuella Ogun <ogunemmanuella2@gmail.com>
2022-10-04 15:30:28 -0700

    program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#define UNUSED(x) (void)(x)
 
 /**
  * main - prints the program name
@@ -7,8 +8,9 @@
  * @argv: pointer to array of arguments
  * Return: Always 0
  */
-int main(int __attribute__((unused)) argc, char **argv)
+int main(int argc, char **argv)
 {
+	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
